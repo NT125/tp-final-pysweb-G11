e7e2dd5a5c2fae677f054e93a369675f5276ae4f
ITorres <ignacio25.it@gmail.com>
2021-06-15 18:28:36 -0300

    Frontend iniciado, pág base creada

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/layout/home/home.component';
+import { AsistenciasComponent } from './components/asistencias/asistencias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    HomeComponent,
+    FooterComponent,
+    HomeComponent,
+    AsistenciasComponent
   ],
   imports: [
     BrowserModule,
