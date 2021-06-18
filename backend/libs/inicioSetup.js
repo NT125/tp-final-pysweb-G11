bcd16430c20a67a01a308f5fcdb9a869d072f80f
DanielRios2002 <65548918+DanielRios2002@users.noreply.github.com>
2021-06-18 03:47:08 -0300

    Agregacion de mas Validaciones hacia todas las Controllers

--- backend/libs/inicioSetup.js
+++ backend/libs/inicioSetup.js
@@ -1,18 +1,17 @@
-const Rol = require( "../models/rol");
+const Rol = require("../models/rol");
 const rolCtrl = {}
 rolCtrl.createRol = async () => {
- 
- try {
-const count = await Rol.estimatedDocumentCount();
-if(count>0)return;
-const values= await Promise.all([
-    new Rol({nombre:'entrenador'}).save(),
-    new Rol({nombre:'alumno'}).save(),
-])
- console.log(values);
- }catch (error) {
-   console.error(error)
-}
+  try {
+    const count = await Rol.estimatedDocumentCount();
+    if (count > 0) return;
+    const values = await Promise.all([
+      new Rol({ nombre: 'entrenador' }).save(),
+      new Rol({ nombre: 'alumno' }).save(),
+    ])
+    console.log(values);
+  } catch (error) {
+    console.error(error)
+  }
 
 };
 module.exports = rolCtrl;
