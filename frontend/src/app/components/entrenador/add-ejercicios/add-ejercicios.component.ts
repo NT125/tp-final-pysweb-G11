9623970f5493378ff3e275307d3f6976b0f0746e
ITorres <ignacio25.it@gmail.com>
2021-06-21 20:22:58 -0300

    Menú rutina rehecho

--- frontend/src/app/components/entrenador/add-ejercicios/add-ejercicios.component.ts
+++ frontend/src/app/components/entrenador/add-ejercicios/add-ejercicios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Ejercicio } from 'src/app/models/ejercicio';
+import { EjercicioService } from 'src/app/services/ejercicio.service';
+import { RutinaService } from 'src/app/services/rutina.service';
 
 @Component({
   selector: 'app-add-ejercicios',
@@ -6,8 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-ejercicios.component.css']
 })
 export class AddEjerciciosComponent implements OnInit {
+  table: boolean;
 
-  constructor() { }
+  ejercicios: Array<any>;
+  ejerciciosRutina: Array<any>;
+
+  constructor(
+  ) {
+    this.ejercicios = new Array<any>();
+    this.table = false;
+    this.ejerciciosRutina = new Array<any>();
+    console.log(this.ejercicios)
+  }
+
+
+  //están bugueadas estas funciones, andaban bien con el ejercicioService
+  addEjerciciosRutina() {
+    console.log(this.ejercicios);
+    this.ejerciciosRutina.push(this.ejercicios);
+    this.ejercicios = new Array<any>();
+    this.table = true;
+  }
+
+  deleteEjercicioRutina(index){
+    this.ejerciciosRutina.splice(index,1);
+  }
+
+  showEjerciciosRutina() {
+    console.log(this.ejerciciosRutina);
+  }
 
   ngOnInit(): void {
   }
--- frontend/src/app/components/entrenador/menu-rutina/menu-rutina.component.ts
+++ frontend/src/app/components/entrenador/menu-rutina/menu-rutina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-rutina',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuRutinaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  //navegación para buttons u otros elementos que no sean <a>
+  goNuevaRutina(){
+    this.router.navigateByUrl("/entrenador/rutina/nueva-rutina");
+  }
+
+  goAddEjercicios(){
+    this.router.navigateByUrl("/entrenador/rutina/add-ejercicios")
+  }
 
   ngOnInit(): void {
   }
