4bcd5d417bb10c5f7d2915a5d0175c1d732b64f7
DanielRios2002 <65548918+DanielRios2002@users.noreply.github.com>
2021-06-23 19:22:48 -0300

    Mejoraas en diseño y agregado de algunas cosas mas

--- frontend/src/app/components/alumno/rutinas/rutinas.component.ts
+++ frontend/src/app/components/alumno/rutinas/rutinas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Ejercicio } from 'src/app/models/ejercicio';
 import { Rutina } from 'src/app/models/rutina';
 import { AlumnoService } from 'src/app/services/alumno.service';
+import { RutinaService } from 'src/app/services/rutina.service';
 
 @Component({
   selector: 'app-rutinas',
@@ -9,9 +11,12 @@ import { AlumnoService } from 'src/app/services/alumno.service';
 })
 export class RutinasComponent implements OnInit {
 
+  bandera: boolean=false;
   rutina: Rutina;
+  idRutina: string;
+  arraydeEjercicios: Array<Ejercicio>;
   arraydeRutinas: Array<Rutina>;
-  constructor(private alumnoserv: AlumnoService) {
+  constructor(private alumnoserv: AlumnoService,private rutinaserv: RutinaService) {
     this.cargarRutinas();
    }
 
@@ -31,4 +36,30 @@ export class RutinasComponent implements OnInit {
     )
     console.log(this.arraydeRutinas);
   }
+
+  verEjercicio(idr: string){
+    this.idRutina=idr;
+    this.bandera=true;
+    this.cargarEjercicios();
+  }
+
+  carcelar(){
+    this.bandera=false;
+  }
+
+  cargarEjercicios(){
+    this.arraydeEjercicios=new Array<Ejercicio>();
+    console.log(this.idRutina);
+    this.rutinaserv.getEjercicios(this.idRutina).subscribe(
+      result=>{
+        result.forEach(element => {
+          let vEjercicio= new Ejercicio();
+          Object.assign(vEjercicio,element);
+          this.arraydeEjercicios.push(vEjercicio);
+        });
+      }
+    )
+    console.log(this.arraydeEjercicios);
+  }
+
 }
